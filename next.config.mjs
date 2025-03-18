7b257c77178991b4adca1665519247b24247c554
OmarElati <5796@holbertonstudents.com>
2025-03-18 03:09:32 +0100

    update static

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            'raw.githubusercontent.com',
+            'opengraph.githubassets.com'
+        ],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'opengraph.githubassets.com',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
